8c95c2235bd602180cacf38e9a6f7f9c4b0b7caa
palmierimayra <palmierimayra@gmail.com>
2022-12-20 11:03:20 -0300

    Nuevo

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -179,7 +179,12 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-
+let arrayMeses [];
+for(i=0;i<=array.length;i++){
+if (arreglo[i]="Enero" || arreglo[i]="Marzo" || arreglo[i]="Noviembre"){
+  arrayMeses[i] === arreglo[i];
+}
+}
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
