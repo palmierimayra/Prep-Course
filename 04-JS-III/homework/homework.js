d40f1aab004172bd44486c64780b093c0f8f954d
palmierimayra <palmierimayra@gmail.com>
2022-12-20 13:45:31 -0300

    Nuevo

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,5 +1,7 @@
 // No cambies los nombres de las funciones.
 
+const { RuleTester } = require("eslint");
+
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
@@ -125,13 +127,25 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  let producto = 1;
+  let i = 0;
+  if (arguments.length > 0){
+     while (arguments[i] >= 0) {
+      producto *= arguments[i];
+      i++; 
+  }
+  return producto;
+  }else if(arguments.length === 0){
+    return 0;
+  } else if(arguments.length === 1){
+  return arguments[0];
 }
-
+}
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
 let contador=0;
-for(i=0;i<=arreglo.length;i++){
+for(let i=0;i<=arreglo.length;i++){
 if(arreglo[i]>18){
   contador=contador+1;
 }
@@ -156,15 +170,18 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+let nString=n.toString();
+if(nString.charAt(0)==9){
+  return true;
+} else {
+  return false;
 }
-
-
+}
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  for(i=0;i<=arreglo.length;i++){
+  for(let i=0;i<=arreglo.length;i++){
     if(arreglo[i]===arreglo[i+1]){
 return true;
     } else{
@@ -179,16 +196,32 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-let arrayMeses [];
-for(i=0;i<=array.length;i++){
-if (arreglo[i]="Enero" || arreglo[i]="Marzo" || arreglo[i]="Noviembre"){
-  arrayMeses[i] === arreglo[i];
-}
+    let arrayMeses=[];
+    let j=0;
+    for(let i=0;i<array.length;i++){
+    if (array[i]=="Enero" || array[i]=="Marzo" || array[i]=="Noviembre"){
+        arrayMeses[j] = array[i];
+        j++;
+      }
+    }
+    if (j!=3){
+        return "No se encontraron los meses pedidos"
+    }
+    return arrayMeses;
 }
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  let masDeCien=[];
+  let j=0;
+  for(let i=0;i<array.length;i++){
+    if(array[i]>100){
+      masDeCien[j] = array[i];
+      j++;
+    }
+  }
+  return masDeCien;
 }
 
 
@@ -200,8 +233,18 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  let array=[];
+  let suma=numero;
+  let sumaTotal=0
+  for(let i=0;i<10;i++){
+ suma = suma + 2;
+array[i] = suma;
+  if(suma===i){
+    return "Se interrumpió la ejecución";
+  }
+  }
+  return array;
 }
-
 
 function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
@@ -210,6 +253,17 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  let array=[];
+  let suma=numero;
+  let j=0;
+  for(let i=0;i<10;i++){
+ if(i!=5){
+  suma = suma + 2;
+array[j] = suma;
+j++;
+}
+  }
+  return array;
 }
 
 
@@ -237,4 +291,4 @@ module.exports = {
   mayorACien,
   breakStatement,
   continueStatement
-};
+}
