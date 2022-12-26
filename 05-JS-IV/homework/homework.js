22973e018101f7b8bdb69292ff3c066a5b4997d3
palmierimayra <palmierimayra@gmail.com>
2022-12-25 21:42:23 -0300

    Nuevo

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,15 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+const gato = {
+  nombre: nombre,
+  edad: edad,
+meow: function() {
+  return "Meow!";
+}
+};
+
+return gato;
 }
 
 function agregarPropiedad (objeto, property) {
@@ -13,6 +22,10 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+objeto[property] = null;
+
+return objeto;
+
 }
 
 function invocarMetodo (objeto, metodo) {
@@ -20,13 +33,14 @@ function invocarMetodo (objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+  return objetoMisterioso.numeroMisterioso*5;
 }
 
 function eliminarPropiedad (objeto, unaPropiedad) {
@@ -34,19 +48,31 @@ function eliminarPropiedad (objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  delete  objeto[unaPropiedad];
+  return objeto;
 }
 
 function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+const usuario = {
+  nombre: nombre,
+  email: email,
+  password: password
+};
+return usuario;
 }
 
 function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if(usuario.email){
+    return true;
+    } else {
+      return false;
+  }
 }
 
 function tienePropiedad (objeto, propiedad) {
